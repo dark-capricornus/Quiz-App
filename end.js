61ce9b379b116ad1f5ba9754f67bf483062a4ecd
Harish <harishdeepabalavenkatraman@gmail.com>
2024-07-03 21:55:26 +0530

    Added essentials

--- end.js
+++ end.js
@@ -1,24 +1,34 @@
-const username = document.getElementById('username');
-const saveScoreBtn =document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById("username") ;
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.olg
 
 finalScore.innerText = mostRecentScore;
+username.addEventListener("keyup" , ()=> {
+    saveScoreBtn.disabled =  ! username.value;
+});    
 
-const highScores = localStorage.setItem('highScores',JSON.stringfy([]));
-console.log(highScores)
-
-
-username.addEventListener("keyup" ,() =>{
-    saveScoreBtn.disabled = !username.value;
-});
-saveHighscore = e =>{
+const saveHighScore  = e => {
     e.preventDefault();
 
-    const score ={
-        score: mostRecentScore,
-        name :username.value
+    const score = {
+        score: Math.floor(Math.random()*100),
+        name : username.value
     };
     highScores.push(score);
-    console.log
+    highScores.sort((a,b)=> b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    window.location.assign("/")
+
+    console.log(highScores);
 };
+
+
+
